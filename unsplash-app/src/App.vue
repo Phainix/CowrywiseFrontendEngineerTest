0bf68307ec30200dd5e2b090235dbbcc072d1cae
Adekogbe Faith <28295747+Phainix@users.noreply.github.com>
2022-01-24 13:33:11 +0100

    Vue App

---
+++ unsplash-app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap');
+@import "App.scss";
+</style>
