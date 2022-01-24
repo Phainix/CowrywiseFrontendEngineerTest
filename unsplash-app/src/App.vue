16d4633128df2eaff950112bfddd3937b287ab33
Adekogbe Faith <28295747+Phainix@users.noreply.github.com>
2022-01-24 13:58:50 +0100

    Landing view & TopBar component

--- unsplash-app/src/App.vue
+++ unsplash-app/src/App.vue
@@ -1,14 +1,8 @@
 <template>
-  <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap");
 @import "App.scss";
 </style>
---
+++ unsplash-app/src/components/TopBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="top-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {},
+})
+export default class TopBar extends Vue {}
+</script>
